aa2eb3cc71b09268a4d7707663bfd517cdba8a9c
Me2Hack <xme2hack@gmail.com>
2014-12-19 13:54:46 +0100

    showframe blahahaha

--- src/userinterface/ShowFrame.java
+++ src/userinterface/ShowFrame.java
@@ -3,6 +3,7 @@ package userinterface;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 
@@ -18,7 +19,7 @@ public class ShowFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel panel;
-	private JLabel lab, lab2,lab3,lab4,lab5,lab6,lab7,labImage;
+	private JLabel lab, lab2,lab3,lab4,lab5,lab6,lab7,labImage,lab8;
 	private Pattern pattern;
 	private GridBagConstraints c = new GridBagConstraints();
 	public ShowFrame(Pattern p) {
@@ -28,38 +29,39 @@ public class ShowFrame extends JFrame {
 		panel = new JPanel();
 		getContentPane().add(panel,BorderLayout.NORTH);
 		panel.setLayout(new GridBagLayout());
+
 		
-		lab = new JLabel(pattern.getName());
+		lab = new JLabel("Name :       "+pattern.getName());
 		c.gridx = 0;
 		c.gridy = 0;
+		c.anchor = GridBagConstraints.WEST;
 		panel.add(lab,c);
-		
-		lab4 = new JLabel(pattern.getContext());
+		lab4 = new JLabel("<html><body style='width: 300 px'>"+"Context:    "+pattern.getContext());
 		c.gridx = 0;
 		c.gridy = 1;
 		panel.add(lab4,c);
 		
-		lab5 = new JLabel(pattern.getProblem());
+		lab5 = new JLabel("<html><body style='width: 300 px'>"+"Problem:      "+pattern.getProblem());
 		c.gridx = 0;
 		c.gridy = 2;
 		panel.add(lab5,c);
 		
-		lab6 = new JLabel(pattern.getSolution());
+		lab6 = new JLabel("<html><body style='width: 300 px'>"+"Solution:                       "+pattern.getSolution());
 		c.gridx = 0;
 		c.gridy = 3;
 		panel.add(lab6,c);
 		
-		lab7 = new JLabel(pattern.getConsequences());
+		lab7 = new JLabel("Consequences:           "+pattern.getConsequences());
 		c.gridx = 0;
 		c.gridy = 4;
 		panel.add(lab7,c);
 		
-		lab2 = new JLabel(pattern.getPurpose().getName());
+		lab2 = new JLabel("Purpose:                        "+pattern.getPurpose().getName());
 		c.gridx = 0;
 		c.gridy = 5;
 		panel.add(lab2,c);
 		
-		lab3 = new JLabel(pattern.getScope().getName());
+		lab3 = new JLabel("Scope:                            "+pattern.getScope().getName());
 		c.gridx = 0;
 		c.gridy = 6;
 		panel.add(lab3,c);
@@ -73,7 +75,7 @@ public class ShowFrame extends JFrame {
 		}catch (Exception e){
 			e.printStackTrace();
 		} 
-		setSize(500, 300);
+		setSize(700, 650);
 		setVisible(true);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	}
