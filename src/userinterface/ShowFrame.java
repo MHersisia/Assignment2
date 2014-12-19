11a62652ecf7c685a912062e53d4b29eca6ca35e
Me2Hack <xme2hack@gmail.com>
2014-12-19 14:35:01 +0100

    ShowFrame GridBag

--- src/userinterface/ShowFrame.java
+++ src/userinterface/ShowFrame.java
@@ -18,7 +18,7 @@ public class ShowFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel panel;
-	private JLabel lab, lab2,lab3,lab4,lab5,lab6,lab7,labImage;
+	private JLabel lab,lab1,lab2,lab3,lab4,lab5,lab6,lab7,labImage,lab8,lab9,lab10,lab11;
 	private Pattern pattern;
 	private GridBagConstraints c = new GridBagConstraints();
 	public ShowFrame(Pattern p) {
@@ -30,28 +30,51 @@ public class ShowFrame extends JFrame {
 		panel.setLayout(new GridBagLayout());
 
 		
-		lab = new JLabel("Name :       "+pattern.getName());
+		lab = new JLabel("Name:");
 		c.gridx = 0;
 		c.gridy = 0;
+		
 		c.anchor = GridBagConstraints.WEST;
 		panel.add(lab,c);
-		lab4 = new JLabel("<html><body style='width: 300 px'>"+"Context:    "+pattern.getContext());
+		lab1 = new JLabel(pattern.getName());
+		c.gridx = 1;
+		c.gridy = 0;
+		panel.add(lab1,c);
+		lab8 = new JLabel("Context:");
 		c.gridx = 0;
 		c.gridy = 1;
+		
+		panel.add(lab8,c);
+		lab4 = new JLabel("<html><body style='width: 300 px'>"+pattern.getContext());
+		c.gridx = 2;
+		c.gridy = 1;
+		c.gridwidth = 300;
 		panel.add(lab4,c);
 		
-		lab5 = new JLabel("<html><body style='width: 300 px'>"+"Problem:      "+pattern.getProblem());
+		lab9 = new JLabel("Problem:");
 		c.gridx = 0;
+		c.gridy = 2;
+		panel.add(lab9,c);
+		lab5 = new JLabel("<html><body style='width: 300 px'>"+"Problem:      "+pattern.getProblem());
+		c.gridx = 2;
 		c.gridy = 2;
 		panel.add(lab5,c);
 		
-		lab6 = new JLabel("<html><body style='width: 300 px'>"+"Solution:                       "+pattern.getSolution());
+		lab10 = new JLabel("Solution:");
 		c.gridx = 0;
+		c.gridy = 3;
+		panel.add(lab10,c);
+		lab6 = new JLabel("<html><body style='width: 300 px'>"+pattern.getSolution());
+		c.gridx = 2;
 		c.gridy = 3;
 		panel.add(lab6,c);
 		
-		lab7 = new JLabel("Consequences:           "+pattern.getConsequences());
+		lab11 = new JLabel("Consequences:");
 		c.gridx = 0;
+		c.gridy = 4;
+		panel.add(lab11,c);
+		lab7 = new JLabel(pattern.getConsequences());
+		c.gridx = 2;
 		c.gridy = 4;
 		panel.add(lab7,c);
 		
